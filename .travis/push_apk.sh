389452e1762cfd77531171ed607175b07ffa81ff
Benjamin Eder <barny.eder@gmail.com>
2018-03-18 14:45:47 +0100

    Potential apk push fix

--- .travis/push_apk.sh
+++ .travis/push_apk.sh
@@ -1,22 +1,26 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
-setup_git() {
-  git config --global user.email "travis@travis-ci.org"
-  git config --global user.name "Travis CI"
-}
+# Create new output directory
+mkdir $HOME/output/
 
-commit_files() {
-  mkdir out
-  cp build/app/outputs/apk/release/app-release.apk out/app-release.apk
-  git add out/app-release.apk
-  git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
-}
+# Copy generated APK from build folder
+cp -R build/app/outputs/apk/release/apk-release.apk $HOME/android/
 
-upload_files() {
-  git remote add origin https://${GitHub}@github.com/mobileappdevhm/calc-BullshitPingu.git > /dev/null 2>&1
-  git push --quiet origin
-}
+# Go to home and setup git
+cd $HOME
+git config --global user.email "travis@travis-ci.org"
+git config --global user.name "Travis CI"
 
-setup_git
-commit_files
-upload_files
+# Clone the repository
+git clone --quiet --branch master = https://BullshitPingu:${GitHub}@github.com/mobileappdevhm/calc-BullshitPingu.git > /dev/null
+
+# Copy needed files to repository
+cd master
+cp -Rf $HOME/android/*.
+
+# Add, commit and push files
+git remote rm origin
+git remote add origin https://BullshitPingu:${GitHub}@github.com/mobileappdevhm/calc-BullshitPingu.git
+git add .
+git commit -m "Travis build: $TRAVIS_BUILD_NUMBER"
+git push origin master -fq > /dev/null
